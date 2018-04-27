90a36bda00b84eac5245d07613de5f857a6a7ba0
Jonathan Perkins <jonathanperkins30@gmail.com>
2018-04-27 17:54:30 +0100

    Create export.php

---
+++ Air Quality/Web Server/Air Monitor/export.php
@@ -0,0 +1,44 @@
+<?php
+/* Variables to Export */
+// database to be exported
+$db_record = 'AirQuality';
+$where = 'WHERE 1 ORDER BY 1';
+// filename for export
+$csv_filename = 'db_export_'.$db_record.'_'.date('Y-m-d').'.csv';
+// database variables
+$hostname = "xxx.xxx.xxx.xx";
+$user = "xxxxxx";
+$password = "xxxxxxx";
+$database = "Jpappsdb";
+// Database connection
+mysql_connect($hostname, $user, $password)
+or die('Could not connect: ' . mysql_error());
+					
+mysql_select_db($database)
+or die ('Could not select database ' . mysql_error());
+// create empty variable to be filled with export data
+$csv_export = '';
+// query to get data from database
+$query = mysql_query("SELECT * FROM ".$db_record." ".$where);
+$field = mysql_num_fields($query);
+// create line with field names
+for($i = 0; $i < $field; $i++) {
+  $csv_export.= mysql_field_name($query,$i).',';
+}
+// newline 
+$csv_export.= '
+';
+// loop through database query and fill export variable
+while($row = mysql_fetch_array($query)) {
+  // create line with field values
+  for($i = 0; $i < $field; $i++) {
+    $csv_export.= '"'.$row[mysql_field_name($query,$i)].'",';
+  }	
+  $csv_export.= '
+';	
+}
+// Export the data and prompt a csv file for download
+header("Content-type: text/x-csv");
+header("Content-Disposition: attachment; filename=".$csv_filename."");
+echo($csv_export);
+?>
